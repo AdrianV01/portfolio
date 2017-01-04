1f6b6cef56b9c92833d217bf116762ec38ed0cbb
Adrian <JS02137@gmail.com>
2017-01-04 08:45:01 -0800

    Began with initial portfolio skeleton

---
+++ app.js
@@ -0,0 +1,4 @@
+'use strict'
+//constructor function pseudo-code, here will be parameters for different
+//endeavors in my life. military, civilian, education, etc. the constructor parameters
+// will include place, period, description, an associated image, and perhaps a link.
