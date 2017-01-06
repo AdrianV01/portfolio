16ee929fea9ed938b80a8f81691eaff03bf4ca3a
Adrian <JS02137@gmail.com>
2017-01-06 08:58:24 -0800

    Added some of the day 2 code to my portfolio and wrecked things with CSS

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 //constructor function pseudo-code, here will be parameters for different
 //endeavors in my life. military, civilian, education, etc. the constructor parameters
 // will include place, period, description, an associated image, and perhaps a link.
+function lifeExcerpt (options) {
+  this.timePeriod = options.timePeriod;
+  this.place = options.place;
+  this.summary = options.summary;
+  this.image = options.image;
+  this.externalLink = options.extenalLink;
+}
